b1eff28549fc4893f4ee2a9c401a48c3e96838ad
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-10-18 16:30:09 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('ACADEMIA_APP', 'doadmin', 'AVNS_HzTD1mdcLG5r73POOIr', {
+const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_HzTD1mdcLG5r73POOIr', {
     dialect: 'mysql',
     host: ' db-mysql-nyc1-46829-do-user-12541529-0.b.db.ondigitalocean.com',
     port: 25060
