6ab520b391cd0ab355dcfc0ecbf433da00578296
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-10-18 17:43:47 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('defaultdb', 'doadmin', 'AVNS_HzTD1mdcLG5r73POOIr', {
     dialect: 'mysql',
-    host: ' db-mysql-nyc1-46829-do-user-12541529-0.b.db.ondigitalocean.com',
+    host: 'db-mysql-nyc1-46829-do-user-12541529-0.b.db.ondigitalocean.com',
     port: 25060
 });
 
