0b88dc47dbabc7cdde95a47b89ab680e8fd5dddc
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-10-24 07:48:22 +0900

    Add files via upload

---
+++ schema/clients.js
@@ -0,0 +1,73 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Clients = database.define('client', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    GYM_NAME: {
+        unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: {    
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        } 
+    }, 
+
+    REPRESENTATIVE: {
+        unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: {    
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        } 
+    },   
+
+    UNIQUE_CODE: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+
+    PASSWORD: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    ADRESS: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    TEL: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+
+    EMAIL: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+   
+    SAVE_DAY: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+   
+    STATUS: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+   
+});
+
+module.exports = Clients;
