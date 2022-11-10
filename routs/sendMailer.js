63d682eb7dbea9a44505aeade6539506c3ec6434
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-11-10 12:41:50 -0300

    atualização do envio de email

--- routs/sendMailer.js
+++ routs/sendMailer.js
@@ -18,23 +18,118 @@ const transporter = nodemailer.createTransport({
  });
 
 module.exports = async function (path, email, name) {
-   
+
+    //variaveis do corpo de envio do email com variação de idiomas para o novo aluno
+    switch (lang) {
+        case 'Portugues':
+            const mailSentPT = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                subject: 'Seja bem vindo!',
+                text: `Olá ${name} tudo bem? Segue em anexo sua ficha de inscrição`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        case 'Inglês':
+            const mailSentEN = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                subject: 'be welcome!',
+                text: `Hello ${name} all right? Attached is your registration form.`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        case '日本語':
+            const mailSentJP = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                subject: '歓迎します!',
+                text: `様今回 ${name} に入会していただきありがとうございます。
+                申し込み申請書をご確認ください。`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        default:
+            console.log("error...")
+    }
+    //---------------------------------------------------------------------------------->
+
+    //variaveis do corpo de envio do email com variação de idiomas para o gerente
+    switch (lang2) {
+        case 'PT':
+            const mailManegerPT = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
+                subject: 'Novo cadastro!',
+                text: `Prezado cliente
+
+                Segue em anexo a ficha de inscrição do novo aluno ${name}.
+                
+                Agradecemos pela parceria e pela preferência.
+                
+                YK technology`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        case 'EN':
+            const mailManegerEN = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
+                subject: 'New customer!',
+                text: `Dear customer
+
+                Attached is the registration form for the new student ${name}.
+                
+                We thank you for your partnership and preference.
+                
+                YK technology`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        case '日本語':
+            const mailManegerJP = await transporter.sendMail({
+                from: '"kledisom" <signatureprojectjp@gmail.com>',
+                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
+                subject: '新しい顧客!',
+                text: `${name}様
+
+                今回入会されました、"nome do aluno"の入会申込書を送付します。
+                
+                今後ともYK technologyとの付き合いをどうぞよろしくお願いいたします。
+                
+                システム不具合やご意見などありましたら、サポートへご連絡いただきまようよろしくお願いいたします。`,
+                attachments: [
+                    {
+                        path: path
+                    }
+                ]
+            });
+            break;
+        default:
+            console.log("error...")
+    };
+    //---------------------------------------------------------------------------------->
 
 
-    const mailSent = await transporter.sendMail({
-        from: '"kledisom" <signatureprojectjp@gmail.com>',
-        to: ['paurozhiyuan@gmail.com', 'kledison2009@hotmail.com', email],
-        subject: 'Seja bem vindo!',
-        text: `Olá ${name} tudo bem? Segue em anexo sua ficha de inscrição`,
-        attachments: [
-            {
-                path: path,
-            }
-        ]
-      
-    });
-
-    return mailSent;
-
-}
+};
 
