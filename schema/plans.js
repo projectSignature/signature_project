2fcf69e9e53b2d101406abada50cb72a6ee36e7c
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-11-15 22:26:24 +0900

    Add files via upload

---
+++ schema/plans.js
@@ -0,0 +1,80 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Plans = database.define('plan', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    GYM_ID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode está vazio.."
+            },
+        }
+    },
+
+    PLANS_NAME: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode está vazio.."
+            },
+        }
+    },
+
+    PLAN_VALOR: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode está vazio.."
+            },
+        }
+    },
+
+    PLAN_KUBUN: {
+        type: Sequelize.STRING(45),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode está vazio.."
+            },
+        }
+    },
+
+    PLAN_DISCRITION1: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+            notEmpty: {
+                msg: "Esse campo não pode está vazio.."
+            },
+        }
+    },
+
+    PLAN_DISCRITION2: {
+        type: Sequelize.STRING(150),
+    },
+
+    PLAN_DISCRITION3: {
+        type: Sequelize.STRING(150),
+    },
+
+    PLAN_DISCRITION4: {
+        type: Sequelize.STRING(150),
+    },
+
+    PLAN_DISCRITION5: {
+        type: Sequelize.STRING(150),
+    },
+
+});
+
+module.exports = Plans;
