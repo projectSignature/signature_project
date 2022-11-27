08c6c693f408bacb583f05a96dd32556785971e5
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-11-27 12:37:11 -0300

    Add files via upload

---
+++ schema/graduation.js
@@ -0,0 +1,91 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Members = database.define('member', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nm_member: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: true,
+        validate: {    
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        } 
+    }, 
+    birthday_year: Sequelize.DECIMAL,
+      
+    birthday_month: Sequelize.DECIMAL,
+    
+    birthday_day: Sequelize.DECIMAL,
+    
+    birthday_age: Sequelize.DECIMAL,
+    
+    genero: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    adress_input: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    phone01: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+
+    phone02: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+
+    phone03: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+
+    email: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+    lang01: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    plans: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    status: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    signature: {
+        type:Sequelize.TEXT,
+        allowNull: false
+    },
+    
+   pass: {
+        type:Sequelize.DECIMAL,
+        allowNull: false
+    },
+    
+    gym: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+    }
+
+
+});
+
+module.exports = Members;
