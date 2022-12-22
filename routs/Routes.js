0236e520ee9eacceb5118ad3358bc544bc9b4574
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-12-22 11:09:03 -0300

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -544,6 +544,47 @@ router.post('/pass', async (req, res) => {
 
 });
 
+//rota para pagamentos
+router.get('/payment/:name/:gym_id/:plan', async (req, res) => {
+  const countMax = await Member.findAll({
+    attributes: [[Sequelize.fn('max', Sequelize.col('id')), 'id']],
+    raw: true,
+  });
+  var month = new Date().getMonth();
+  var year = new Date().getFullYear();
+  var param = req.params;
+
+  var arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  for (let i = (month - 1); i < arr.length; i++) {
+
+    arr[i] = 1;
+  }
+
+  await database.sync();
+  let newPay = await Pay.create({
+    nm_member_id: countMax[0].id,
+    nm_member: param.name,
+    year: year,
+    jan: arr[0],
+    feb: arr[1],
+    mar: arr[2],
+    abr: arr[3],
+    may: arr[4],
+    jun: arr[5],
+    jul: arr[6],
+    aug: arr[7],
+    sep: arr[8],
+    oct: arr[9],
+    nov: arr[10],
+    dez: arr[11],
+    obs: 0,
+    plan: param.plan,
+    GYM_ID: param.gym_id
+  });
+
+  res.json(newPay);
+});
+
 module.exports = router;
 
 
