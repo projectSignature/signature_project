22e28a20020d212b21ed89782a8101ca0f57bbc3
projectSignature <116049808+projectSignature@users.noreply.github.com>
2022-12-30 11:48:07 +0900

    Update sendMailer.js

--- routs/sendMailer.js
+++ routs/sendMailer.js
@@ -9,22 +9,22 @@ const transporter = nodemailer.createTransport({
     secure: false,
     auth: {
         user: SMTP_CONFIG.auth.user,
-        pass: SMTP_CONFIG.auth.pass  
-       
+        pass: SMTP_CONFIG.auth.pass
+
     },
     tls: {
         rejectUnauthorized: false,
-    } 
+    }
  });
 
-module.exports = async function (path, email, name, lang, lang2, email2) {
+module.exports = async function (path, email, name, lang, lang2, email2, gymname) {
 
     //variaveis do corpo de envio do email com variação de idiomas para o novo aluno
     switch (lang) {
         case 'Portugues':
             const mailSentPT = await transporter.sendMail({
-                from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                from: `"${gymname}" <signatureprojectjp@gmail.com>`,
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'
                 subject: 'Seja bem vindo!',
                 text: `Olá ${name} tudo bem? Segue em anexo sua ficha de inscrição`,
                 attachments: [
@@ -36,8 +36,8 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
             break;
         case 'Inglês':
             const mailSentEN = await transporter.sendMail({
-                from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                from: `"${gymname}" <signatureprojectjp@gmail.com>`,
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'
                 subject: 'be welcome!',
                 text: `Hello ${name} all right? Attached is your registration form.`,
                 attachments: [
@@ -49,11 +49,11 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
             break;
         case '日本語':
             const mailSentJP = await transporter.sendMail({
-                from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email],
+                from: `"${gymname}" <signatureprojectjp@gmail.com>`,
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'mudar para email
                 subject: '歓迎します!',
-                text: `様今回 ${name} に入会していただきありがとうございます。
-                申し込み申請書をご確認ください。`,
+                text: `${name} 様, 今回入会していただきありがとうございます。
+                申請書をご確認ください。`,
                 attachments: [
                     {
                         path: path
@@ -70,15 +70,15 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
     switch (lang2) {
         case 'PT':
             const mailManegerPT = await transporter.sendMail({
-                from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
+                from: '"YK technology" <signatureprojectjp@gmail.com>',
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'
                 subject: 'Novo cadastro!',
                 text: `Prezado cliente
 
                 Segue em anexo a ficha de inscrição do novo aluno ${name}.
-                
+
                 Agradecemos pela parceria e pela preferência.
-                
+
                 YK technology`,
                 attachments: [
                     {
@@ -89,15 +89,15 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
             break;
         case 'EN':
             const mailManegerEN = await transporter.sendMail({
-                from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
+                from: '"YK technology" <signatureprojectjp@gmail.com>',
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'
                 subject: 'New customer!',
                 text: `Dear customer
 
-                Attached is the registration form for the new student ${name}.
-                
+                Attached is the registration form for the new member ${name}.
+
                 We thank you for your partnership and preference.
-                
+
                 YK technology`,
                 attachments: [
                     {
@@ -109,14 +109,14 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
         case '日本語':
             const mailManegerJP = await transporter.sendMail({
                 from: '"kledisom" <signatureprojectjp@gmail.com>',
-                to: ['kedinhofavorito@gmail.com', 'kledison2009@hotmail.com', email2],
-                subject: '新しい顧客!',
+                to: ['paurozhiyuan@gmail.com', email2], //'kedinhofavorito@gmail.com', 'kledison2009@hotmail.com'
+                subject: '入会者の連絡!',
                 text: `${name}様
 
                 今回入会されました、"nome do aluno"の入会申込書を送付します。
-                
+
                 今後ともYK technologyとの付き合いをどうぞよろしくお願いいたします。
-                
+
                 システム不具合やご意見などありましたら、サポートへご連絡いただきまようよろしくお願いいたします。`,
                 attachments: [
                     {
@@ -132,4 +132,3 @@ module.exports = async function (path, email, name, lang, lang2, email2) {
 
 
 };
-
