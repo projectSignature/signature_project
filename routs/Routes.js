0228b4e860ce33b6c2e26a625db035eea66db57c
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-01-03 11:32:11 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -204,7 +204,7 @@ router.get('/paymentall', async (req, res) => {
 
 
 //read de dados do pay------------------------------->
-router.get('/paymentallexist', async (req, res) => {
+router.post('/paymentallexist', async (req, res) => {
   console.log('in')
   console.log(req.body)
    try{
