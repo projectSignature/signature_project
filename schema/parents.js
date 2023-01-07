9f2424a576819074c432a3c137fa59536bb90891
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-01-07 09:42:07 -0300

    Add files via upload

---
+++ schema/parents.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Parents = database.define('parent', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    family_name: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    birthday: Sequelize.STRING(150),
+
+    birthday_age: Sequelize.STRING(150),
+
+    genero: {
+        type:Sequelize.STRING(150),
+        allowNull: false
+    },
+
+     gymid: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+    },
+
+    nm_member_id: {
+        type: Sequelize.INTEGER,
+         allowNull: false,
+    }
+
+
+});
+
+module.exports = Parents;
