8a31cb0539541471806ec59a41da7ec037e72ebc
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-01-21 18:27:42 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -787,13 +787,17 @@ router.get('/calender/entrance', async (req, res) => {
 //Rota para registrar entradas da entrance
 router.post('/registerEntrance', async (req, res) => {
   try {
+    const japanStandardTime = new Date(Date.now() + ((new Date().getTimezoneOffset() + (9 * 60)) * 60 * 1000));
+    const japanweekeday = japanStandardTime.getDay()
     await database.sync();
 
     const newEntrance = await Entrance.create({
       LESSON_NAME: req.body.LESSON_NAME,
       LESSON_HOUR: req.body.LESSON_HOUR,
       MEMBER_ID: req.body.MEMBER_ID,
-      GYM_ID: req.body.GYM_ID
+      GYM_ID: req.body.GYM_ID,
+      LESSON_DATE: japanStandardTime,
+      LESSON_DAY: japanweekeday,
     });
 
     res.json(newEntrance)
