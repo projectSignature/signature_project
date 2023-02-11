25d1f10a6818b5a557f812f6b49d7a185d5bbb05
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-02-12 00:41:06 +0900

    Add files via upload

---
+++ schema/finencepays.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Finencepays = database.define('finencepay', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+
+    GYM_ID: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+    },
+
+    CATEGORY: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+    },
+
+    NAME: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+    },
+
+    VALUE: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+    },
+
+    COLOR: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+    },
+
+    Date: {
+        type: Sequelize.STRING(150),
+        allowNull: false,
+    },
+
+});
+
+module.exports = Finencepays;
