c7c8bcc154e80fba07477fa6c221303d58c20907
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-02-18 18:20:27 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -1111,6 +1111,11 @@ router.post('/planDelete', async (req, res) => {
   res.json('deletado');
 })
 
+router.post('/expenseHistoryDelete', async (req, res) => {
+  Expense.destroy({ where: { id: req.body.id } })
+  res.json('deletado');
+})
+
 //gerar PDF inscrição
 router.get('/pdf', async (req, response) => {
 try{
