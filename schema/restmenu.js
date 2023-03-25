3d1e8bd5c887e369ca81e6d779089df0c47a7292
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-03-25 09:55:28 -0300

    Add files via upload

---
+++ schema/restmenu.js
@@ -0,0 +1,71 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const RestMenu = database.define('restMenu', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    menu_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    menu_child_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    menu_name_0: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    menu_name_1: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    menu_name_2: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    menu_value: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    rest_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = RestMenu;
