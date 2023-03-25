dc962d8cc612b5e162672bb544b343e5360efbdd
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-03-24 22:24:39 -0300

    Add files via upload

---
+++ schema/rest_clients.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const RestClients = database.define('restClient', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    name: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    phone: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    adress: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    post: {unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        unique: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+});
+
+module.exports = RestClients;
