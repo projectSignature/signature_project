142d968ea8eb3cb0fdfb92d6fdd7e3fc38ccd679
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-03-26 16:20:08 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -1447,12 +1447,11 @@ router.get('/menuGet', async (req, res) => {
   console.log(req.query.menuid)
    try{
   const menuAnswer = await RestMenu.findAll({
-  //  where: {
-   //   rest_id: req.query.id,
-    //  menu_id: req.query.menuid
-  //  }
+   where: {
+   rest_id: req.query.id,
+   menu_id: req.query.menuid
+    }
   });
-
   res.json(menuAnswer)
 } catch(err) {
   console.log(err)
