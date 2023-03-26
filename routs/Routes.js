3f2ffb7112d0b58761adf70aa1c7dd58b0a8f94a
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-03-26 15:30:26 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -1441,7 +1441,7 @@ router.post('/authClients', async (req, res) => {
 });
 
 //rest app menu Get------------------------>
-router.get('/restmenu', async (req, res) => {
+router.get('/menuGet', async (req, res) => {
   console.log("in")
    try{
   const menuAnswer = await RestMenu.findAll({
