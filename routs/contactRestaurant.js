6f8f20b67e724837dbb94c26958f5119c9c8a512
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-04-05 11:23:56 -0300

    Add files via upload

---
+++ routs/contactRestaurant.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+const SMTP_CONFIG = require('./config/smtp');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    auth: {
+        user: SMTP_CONFIG.auth.user,
+        pass: SMTP_CONFIG.auth.pass
+
+    },
+    tls: {
+        rejectUnauthorized: false,
+    }
+});
+
+module.exports = async function restaurant(name,tel,email,mens) {
+
+    //variaveis do corpo de envio do email com variação de idiomas para o novo aluno
+
+      const mailSentPT = await transporter.sendMail({
+        from: '"YK technology"" <signatureprojectjp@gmail.com>',
+        to: ['paurozhiyuan@gmail.com','rootsgrillhekinan@gmail.com'],
+        subject: `Aviso sobre contato pelo website`,
+        text: `Prezado cliente
+
+        ${nome} entrou em contato atrávez to web site.
+
+        Nome:${nome}
+        Telefone:${tel}
+        E-mail:${email}
+        Menssagem:${mens}
+
+        YK technology`,
+    });
+};
