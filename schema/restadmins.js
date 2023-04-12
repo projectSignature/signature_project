259cfbfe2f00e540b12cf4cc39b5564774a7791c
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-04-12 11:12:47 -0300

    Add files via upload

---
+++ schema/restadmins.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Restadmins = database.define('restadmin', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    rest_id: {
+        unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    worker_name: {
+        unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    adress: {
+        unique: false,
+        type: Sequelize.STRING(150),
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    password: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING(150),
+        allowNull: false
+    },
+});
+
+module.exports = Restadmins;
