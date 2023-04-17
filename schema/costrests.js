ffd8029962c75d462d24257bd9a8ba3dd1f62438
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-04-17 10:49:44 -0300

    Add files via upload

---
+++ schema/costrests.js
@@ -0,0 +1,79 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Costrests = database.define('costrest', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    rest_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    worker_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    cost_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    amount: {
+        type: Sequelize.DECIMAL(15,2),
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    paydat:{
+      type: Sequelize.DATE,
+      validate: {
+       notEmpty: {
+          msg: "Esse campo não pode está vazio.."
+       },
+      }
+    },
+    memo:{
+      type:Sequelize.STRING(200),
+      allowNull: false
+    },
+    paykubun: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+    status: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+         notEmpty: {
+            msg: "Esse campo não pode está vazio.."
+         },
+        }
+    },
+});
+
+module.exports = Costrests;
