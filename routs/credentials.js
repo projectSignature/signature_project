9520aa5243ae3a109c7d421743ff2de578033659
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-06-01 08:59:31 -0300

    Add files via upload

--- routs/credentials.js
+++ routs/credentials.js
@@ -2,8 +2,16 @@
 
 //+------------------------------------------------------------------------------------+
 const credentials = {
-    squareApplicationId: 'sandbox-sq0idb-0A10Jf4WOIi2NgJQ17LZMQ',
-    accessToken: 'EAAAF3ZlmIUK5DR7uZ5tidme48j06k3E6TPhhaWKaC5QsDjtTdk6TTG_Q7eGIxZB'
+    production: {
+        accessToken: 'EAAAF3ZlmIUK5DR7uZ5tidme48j06k3E6TPhhaWKaC5QsDjtTdk6TTG_Q7eGIxZB',
+        locationId: 'L8ZK9T0BNEATZ',
+    },
+    sandbox: {
+        squareApplicationId: 'sandbox-sq0idb-0A10Jf4WOIi2NgJQ17LZMQ',
+        accessToken: 'EAAAEK3nRLSUgZYRM2p6zM_zagPzjK8Rz-Yv9kWT25NdHlKroUKluPVFuAv1L3gG',
+        locationId: 'LCQMDCCX3TX66',
+    }
+
 };
 //+------------------------------------------------------------------------------------+
 module.exports = credentials;
