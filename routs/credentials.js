38219dc9be0eb3e0705bea6c4bd70cebc81be33f
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-06-01 08:51:23 -0300

    Add files via upload

---
+++ routs/credentials.js
@@ -0,0 +1,9 @@
+//credentials.js
+
+//+------------------------------------------------------------------------------------+
+const credentials = {
+    squareApplicationId: 'sandbox-sq0idb-0A10Jf4WOIi2NgJQ17LZMQ',
+    accessToken: 'EAAAF3ZlmIUK5DR7uZ5tidme48j06k3E6TPhhaWKaC5QsDjtTdk6TTG_Q7eGIxZB'
+};
+//+------------------------------------------------------------------------------------+
+module.exports = credentials;
