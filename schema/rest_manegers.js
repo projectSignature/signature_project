b0b3e97b42905d2fb276c9ed1c72f94273bec7c5
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-06-07 11:07:41 -0300

    Add files via upload

---
+++ schema/rest_manegers.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Rest_manegers = database.define('rest_maneger', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    rest_id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+    },
+    work_status: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+    },
+    pickup_time: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+    },
+
+});
+
+module.exports = Rest_manegers;
