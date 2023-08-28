5b15a783aba0ee3b4ec8000c5faee44b7bb5bc5f
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-08-29 00:13:40 +0900

    Update Routes.js

--- routs/Routes.js
+++ routs/Routes.js
@@ -254,6 +254,20 @@ router.post('/createCostRest', async (req, res) => {
   }
 });
 
+router.post('/dakokuistfinaldata', async (req, res) => {
+  try {
+    const newClient = await Dakoku.create({
+      worker_id: req.body.d1,
+      fn: req.body.d2,
+      workday: req.body.d3
+    });
+    res.json(newClient);
+  } catch (err) {
+    console.log(err)
+    return res.status(400).json(err)
+  }
+});
+
 router.post('/dakokuistdata', async (req, res) => {
   try {
     const newClient = await Dakoku.create({
