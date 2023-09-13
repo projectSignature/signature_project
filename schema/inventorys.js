5f7f455f3c7df4dbc913a9196be7e6b9483337c4
projectSignature <116049808+projectSignature@users.noreply.github.com>
2023-09-13 11:51:27 -0300

    Add files via upload

---
+++ schema/inventorys.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const database = require('../db');
+
+const Inventorys = database.define('inventory', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    rest_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    name: {
+      type: Sequelize.STRING(45),
+      unique: false,
+    },
+    quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    cust: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    updt: {
+      type: Sequelize.STRING(45),
+      unique: false,
+    }
+
+});
+
+module.exports = Inventorys;
