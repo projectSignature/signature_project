1f6a87c75b74a4c2feae8bfd16d8e167aa186fef
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-08-05 15:27:37 +0900

    Add files via upload

---
+++ posdb.js
@@ -0,0 +1,8 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('pos_db', 'doadmin', 'AVNS_HzTD1mdcLG5r73POOIr', {
+    dialect: 'mysql',
+    host: 'db-mysql-nyc1-46829-do-user-12541529-0.b.db.ondigitalocean.com',
+    port: 25060
+});
+
+module.exports = sequelize;
