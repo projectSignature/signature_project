ba3ce51e6d649b93c1f1ced6761453f2bfe7a262
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-10-02 10:02:13 +0900

    Update reservation.service.js

--- services/reservation.service.js
+++ services/reservation.service.js
@@ -1,11 +1,13 @@
-const Reservation = require('../schema/orders/Reservation');  // 予約モデルをインポート
+const Reservation = require('../schema/orders/reservation');  // 予約モデルをインポート
 
 const reservationService = {
     // 全ての予約を取得
-    getAll: async (userId) => {
-      console.log('予約確認に入った')
+    getAll: async (userId,reservation_date) => {
+
         return await Reservation.findAll({
-            where: { user_id: userId }
+            where: {
+              user_id: userId,
+              reservation_date:reservation_date  }
         });
     },
 
