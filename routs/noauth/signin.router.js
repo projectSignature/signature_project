50d6ab5c4177435602b211168bba930dd7ed4096
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-10-05 10:26:05 +0900

    Update signin.router.js

--- routs/noauth/signin.router.js
+++ routs/noauth/signin.router.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const signinController = require('../../controllers/signin.controller')
 const signinPosController = require('../../controllers/pos.signin.controller')
+const signinOrderController = require('../../controllers/orderskun.signin.controller')
 
 const signinRouter = Router()
 
 signinRouter.post('/signin', signinController.signin)
 signinRouter.post('/pos/signin', signinPosController.signin)
-
+signinRouter.post('/orders/signin', signinOrderController.signin)
 module.exports = signinRouter
