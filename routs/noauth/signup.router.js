a98c0ab246a40d2298b597268f59595d2fb5f23c
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-10-05 10:26:34 +0900

    Update signup.router.js

--- routs/noauth/signup.router.js
+++ routs/noauth/signup.router.js
@@ -1,8 +1,10 @@
 const { Router } = require('express')
 const signupController = require('../../controllers/signup.controller')
+const OrderssignupController = require('../../controllers/orders.signup.controller')
 
 const signupRouter = Router()
 
 signupRouter.post('/signup', signupController.signup)
+signupRouter.post('/orders/signup', OrderssignupController.signup)
 
 module.exports = signupRouter
