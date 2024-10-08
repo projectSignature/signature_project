dc37d0396f0dab2ac91abd5241bb437b8ae55fed
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-10-08 22:41:15 +0900

    Update menu.js

--- schema/pos/menu.js
+++ schema/pos/menu.js
@@ -17,7 +17,15 @@ const Menu = database.define('Menu', {
         type: DataTypes.STRING(255),
         allowNull: false
     },
+    item_name_jp: {  // 日本語のアイテム名
+        type: DataTypes.STRING(255),
+        allowNull: true  // 必須ではない場合は allowNull: true にする
+    },
     category: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+    },
+    category_jp: {  // 日本語のカテゴリ名
         type: DataTypes.STRING(255),
         allowNull: true
     },
@@ -33,6 +41,11 @@ const Menu = database.define('Menu', {
         type: DataTypes.BOOLEAN,
         allowNull: false,
         defaultValue: true
+    },
+    isVisible: {  // 表示・非表示を管理するカラムを追加
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true  // デフォルトで表示されるように設定
     }
 }, {
     tableName: 'Menu',  // テーブル名を指定
