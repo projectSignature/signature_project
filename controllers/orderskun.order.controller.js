6a5f0381bdeac275a2040d0da0f9970e26457597
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-10-11 10:56:20 +0900

    Update orderskun.order.controller.js

--- controllers/orderskun.order.controller.js
+++ controllers/orderskun.order.controller.js
@@ -3,19 +3,17 @@ const orderService = require('../services/orders.service');
 
 const orderController = {
     confirmOrder: async (req, res) => {
-      console.log('new orderkuns')
         const { order_name, user_id, table_no, items, orderId } = req.body;
-    console.log(order_name, user_id, table_no, items, orderId )
         try {
             // 既存の注文を確認
             let result = await orderService.updateExistingOrder(user_id, table_no, order_name, items, orderId);
-
             if (!result.success) {
                 // 新規注文を作成
                 result = await orderService.createNewOrder(user_id, table_no, order_name, items);
+                  res.status(200).json({ message: result.message,newflug: true, order: result.order, orderItems:result.orderItems});
+            }else{
+              res.status(200).json({ message: result.message,newflug: false});
             }
-
-            res.status(200).json({ message: result.message });
         } catch (error) {
             console.error('Error confirming order:', error);
             res.status(500).json({ error: 'Failed to confirm order' });
@@ -35,7 +33,6 @@ const orderController = {
    updateOrderStatus: async (req, res) => {
        const { orderItemId, newStatus } = req.body;  // フロントから送信されたデータを取得
        try {
-         console.log(orderItemId, newStatus)
            const result = await orderService.updateOrderItemStatus(orderItemId, newStatus);
            res.status(200).json({ message: result.message });
        } catch (error) {
@@ -49,7 +46,6 @@ const orderController = {
        try {
            // サービスを呼び出して alarm_enabled を更新
            const result = await orderService.updateAlarmStatus(orderId, alarmStatus);
-
            if (result.success) {
                res.status(200).json({ message: 'Alarm status updated successfully' });
            } else {
@@ -62,7 +58,6 @@ const orderController = {
    },
    deleteOrder: async (req, res) => {
     const orderId = req.params.orderId; // パラメータから orderId を取得
-
     try {
       // サービス層を呼び出してオーダーとアイテムを削除
       const result = await orderService.deleteOrder(orderId);
@@ -70,15 +65,12 @@ const orderController = {
       if (!result.success) {
         return res.status(404).json({ message: result.message });
       }
-
       res.status(200).json({ message: result.message });
-
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: '注文の削除中にエラーが発生しました' });
     }
   }
-
 };
 
 module.exports = orderController;
