a6934baf7fd2fa53faced4cfe271e298848e6ddf
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-12-02 15:46:59 +0900

    Add files via upload

---
+++ registerDb.js
@@ -0,0 +1,10 @@
+const Sequelize = require('sequelize');
+
+
+const sequelize = new Sequelize('keirikun_register', 'doadmin', 'AVNS_HzTD1mdcLG5r73POOIr', {
+    dialect: 'mysql',
+    host: 'db-mysql-nyc1-46829-do-user-12541529-0.b.db.ondigitalocean.com',
+    port: 25060
+});
+
+module.exports = sequelize
