436d85e3f2199b274f5e787c3a331470912ad151
projectSignature <116049808+projectSignature@users.noreply.github.com>
2024-12-23 03:45:13 +0900

    Add files via upload

---
+++ schema/imagesSystem/uploadImageAccess.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../imageUpload_db');  // データベース接続をインポート
+
+const ImageUpload = sequelize.define('ImageUpload', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    image_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    date: {
+        type: DataTypes.DATEONLY, // 日付のみ
+        allowNull: false,
+    },
+    comment: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+    },
+}, {
+    tableName: 'uploadImageAccess', // テーブル名
+    timestamps: true, // createdAtとupdatedAtを自動管理
+});
+
+module.exports = ImageUpload;
