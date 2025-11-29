575a166cc096fa20734ff6904c9303f75ed95c5f
projectSignature <116049808+projectSignature@users.noreply.github.com>
2025-11-30 02:21:42 +0900

    Update DailyRoomList.js

--- schema/roomSync/DailyRoomList.js
+++ schema/roomSync/DailyRoomList.js
@@ -13,37 +13,83 @@ const DailyRoomList = database.define(
       type: DataTypes.INTEGER,
       allowNull: false,
     },
+
     work_date: {
       type: DataTypes.DATEONLY,
       allowNull: false,
     },
+
     room_number: {
       type: DataTypes.STRING(50),
       allowNull: false,
     },
+
+    // ⭐ floor を追加
+    floor: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+
     status: {
       type: DataTypes.STRING(50),
       allowNull: true,
     },
+
     guest_count: {
       type: DataTypes.INTEGER,
       allowNull: true,
       defaultValue: 0,
     },
 
-    // 🔹 追加分
+    sub_status: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+    },
+
+    amenity_only: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+    notes: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    open_flag: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+    checked_flag: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+    amenity_complete_flag: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+
     assigned_staff_id: {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
+
     cleaned_by: {
       type: DataTypes.INTEGER,
       allowNull: true,
     },
+
     cleaning_done_at: {
       type: DataTypes.DATE,
       allowNull: true,
     },
+
     checked_done_at: {
       type: DataTypes.DATE,
       allowNull: true,
@@ -54,15 +100,33 @@ const DailyRoomList = database.define(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
+
     updated_at: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
+
     checkout_time: {
-  type: DataTypes.DATE,
-  allowNull: true,
-},
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+
+    is_edited: {
+      type: DataTypes.TINYINT,
+      allowNull: false,
+      defaultValue: 0,
+    },
+
+    edit_history: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+
+    stay_type: {
+      type: DataTypes.STRING(20),
+      allowNull: true,
+    },
   },
   {
     tableName: "daily_room_list",
