c40c571f5afd99b1235318d175c3311be4053539
projectSignature <116049808+projectSignature@users.noreply.github.com>
2026-01-14 16:02:49 +0900

    Add files via upload

---
+++ schema/orders/MenuCostHistory.js
@@ -0,0 +1,44 @@
+const { DataTypes } = require('sequelize');
+const database = require('../../order_db');
+
+const MenuCostHistory = database.define('MenuCostHistory', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+
+  rest_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: 'レストランID'
+  },
+
+  menu_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    comment: 'menu.id（論理的関連）'
+  },
+
+  cost_price_ex_tax: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+    comment: '税抜原価（1皿あたり）'
+  },
+
+  start_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+
+  end_date: {
+    type: DataTypes.DATEONLY,
+    allowNull: true
+  }
+}, {
+  tableName: 'menu_cost_history',
+  timestamps: true,
+  underscored: true
+});
+
+module.exports = MenuCostHistory;
